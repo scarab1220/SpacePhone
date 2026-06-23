948650d805aeec539a1060f2abc00ff808661154
Oscar Abarca <starlightrevolutionx@icloud.com>
2026-06-22 23:39:25 -0600

    Add Vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,11 @@
 import { defineConfig } from "vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
-import netlify from "@netlify/vite-plugin-tanstack-start";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
 export default defineConfig({
-  plugins: [tanstackStart(), netlify(), react(), tailwindcss()],
+  plugins: [tanstackStart(), react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
